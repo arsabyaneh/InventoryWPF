ac4a76148f87a278ca058a9177bf38b385648a69
arsabyaneh <63109912+arsabyaneh@users.noreply.github.com>
2022-02-11 17:23:53 +0100

    improved navigation

--- Inventory.Core/Stores/NavigationStore.cs
+++ Inventory.Core/Stores/NavigationStore.cs
@@ -10,6 +10,7 @@ namespace Inventory.Core.Stores
     public class NavigationStore
     {
         private BaseViewModel? _CurrentViewModel;
+        private Stack<BaseViewModel?> _ViewModelsStack = new Stack<BaseViewModel?>();
 
         public BaseViewModel? CurrentViewModel
         {
@@ -25,12 +26,28 @@ namespace Inventory.Core.Stores
 
         private void OnCurrentViewModelChanged()
         {
+            if (CurrentViewModel != null)
+                _ViewModelsStack.Push(_CurrentViewModel);
+
             CurrentViewModelChanged?.Invoke();
         }
 
         public void Close()
         {
-
+            if (_ViewModelsStack.Count >= 2)
+            {
+                _ViewModelsStack.Pop();
+                CurrentViewModel = _ViewModelsStack.Peek();
+            }
+            else if (_ViewModelsStack.Count == 1)
+            {
+                _ViewModelsStack.Pop();
+                CurrentViewModel = null;
+            }
+            else
+            {
+                CurrentViewModel = null;
+            }
         }
     }
 }
